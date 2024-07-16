97454e35981d3c89697842c4166f108024a470ff
Leo Zhang <leozhang53@gmail.com>
2024-07-15 23:59:14 -0700

    setup

--- app/app.py
+++ app/app.py
@@ -1,27 +1,10 @@
 from flask import Flask
-import psycopg2
-from psycopg2.extras import RealDictCursor
 
 app = Flask(__name__)
 
-def get_db_connection():
-    conn = psycopg2.connect(
-        host='postgres',
-        database='mydatabase',
-        user='myuser',
-        password='mypassword'
-    )
-    return conn
-
 @app.route('/')
 def hello():
-    conn = get_db_connection()
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    cursor.execute('SELECT count(*) FROM mytable')
-    count = cursor.fetchone()['count']
-    cursor.close()
-    conn.close()
-    return f"Hello, World! The count is {count}."
+    return "Almeida is king."
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5002)
+    app.run(debug=True, host="0.0.0.0", port=5002)
