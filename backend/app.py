314218cc81a55946100427834d53189a483e5b5f
Leo Zhang <leozhang53@gmail.com>
2024-08-14 23:32:16 -0400

    modify

--- backend/app.py
+++ backend/app.py
@@ -19,12 +19,12 @@ def about():
 # Contact page route
 @app.route('/contact')
 def contact():
-    return "Contact!"
+    return "#FakhretdinovAndNew"
 
 # Another page route
 @app.route('/services')
 def services():
-    return "Services"
+    return "BIGI BOY!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3001)
